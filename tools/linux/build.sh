98cfa92684c5a6d58876ebdb013c736722772cea
Marc Pignat <marc@pignat.org>
2016-02-29 08:21:41 +0100

    debian package: automatic today version

--- tools/linux/build.sh
+++ tools/linux/build.sh
@@ -3,6 +3,7 @@ DIR=`mktemp -d`
 DATE=`date -I`
 
 cp -a hades-tools $DIR/hades-tools-$DATE
+cat $DIR/hades-tools-$DATE/DEBIAN/control.in | sed -e "s/Version:/Version:$DATE/" > $DIR/hades-tools-$DATE/DEBIAN/control
 dpkg-deb -b $DIR/hades-tools-$DATE
 mv $DIR/hades-tools-$DATE.deb .
 rm -r $DIR
