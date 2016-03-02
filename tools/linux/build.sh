17bf1543e1bcf9f9dea625ebb7c690af44a85be3
Marc Pignat <marc@pignat.org>
2016-03-02 16:05:09 +0100

    fix some litian warnings and errors

--- tools/linux/build.sh
+++ tools/linux/build.sh
@@ -4,6 +4,6 @@ DATE=`date -I`
 
 cp -a hades-tools $DIR/hades-tools-$DATE
 cat $DIR/hades-tools-$DATE/DEBIAN/control.in | sed -e "s/Version:/Version:$DATE/" > $DIR/hades-tools-$DATE/DEBIAN/control
-dpkg-deb -b $DIR/hades-tools-$DATE
+fakeroot ./_fakeroot.sh $DIR/hades-tools-$DATE
 mv $DIR/hades-tools-$DATE.deb .
 rm -r $DIR
